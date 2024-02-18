bd48458677ce520cd720b11a137b227517f7cf4e
sfedoss <f.savolikov@yandex.ru>
2024-02-18 21:01:12 +0300

    поправил игру

--- rememberWords.js
+++ rememberWords.js
@@ -1,18 +1,24 @@
 function rememberWords() {
     let wordArray = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
     wordArray = wordArray.sort(() => Math.random() - 0.5);
-
+  
     alert(wordArray);
-
-    let answerOne = prompt('Что было первым в списке?').toLowerCase();
-
-    let answerTwo = prompt('Что было последним в списке?').toLowerCase();
-
-    if (wordArray[0].toLowerCase() === answerOne && wordArray[wordArray.length - 1].toLowerCase() === answerTwo) {
-        return 'Молодец, хорошая память!';
-    } else if (wordArray[0].toLowerCase() === answerOne || wordArray[wordArray.length - 1].toLowerCase() === answerTwo) {
-        return 'Ты был близок!';
-    } else {
+  
+    let answerOne = prompt('Что было первым в списке?');
+  
+    let answerTwo = prompt('Что было последним в списке?');
+    
+    if (typeof answerOne === 'object') {
         return 'Не угадал';
     }
-}
+  
+    if (wordArray[0].toLowerCase() === answerOne.toLowerCase() && wordArray[wordArray.length - 1].toLowerCase() === answerTwo.toLowerCase()) {
+      return 'Молодец, хорошая память!';
+    } else if (wordArray[0].toLowerCase() === answerOne.toLowerCase() || wordArray[wordArray.length - 1].toLowerCase() === answerTwo.toLowerCase()) {
+      return 'Ты был близок!';
+    } else {
+      return 'Не угадал';
+    }
+  }
+  
+  console.log(rememberWords());
